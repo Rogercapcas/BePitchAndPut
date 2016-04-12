a5907fc2249eb91b546508855d6a12de73dd380e
“Daniel <danigv19@gmail.com>
2016-04-12 17:21:21 +0200

    Primeres modificacions models

--- BePitchAndPutt/iBePitchAndPutt/models.py
+++ BePitchAndPutt/iBePitchAndPutt/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Player (models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    handicap = models.FloatField()
+
+class Match (models.Model):
+
+    hole = models.ForeignKey(Hole)
+    hole_result = models.IntegerField()
+    total_result = models.IntegerField()
+
+class Hole (models.Model):
+    field = models.ForeignKey(Field)
+    hole_number = models.IntegerField()
+    meters = models.IntegerField()
+    handicap_hole = IntegerField()
+
+
+class Field(models.Model):
+    holes = models.IntegerField()
+    par = models.IntegerField()
