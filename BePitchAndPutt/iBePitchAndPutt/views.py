742e99cc65184bc0c3cdbdd50d7de3845b83a59c
“Daniel <danigv19@gmail.com>
2016-04-14 17:47:15 +0200

    Flitrat per jugador i match

--- BePitchAndPutt/iBePitchAndPutt/views.py
+++ BePitchAndPutt/iBePitchAndPutt/views.py
@@ -33,14 +33,15 @@ def mainpage(request):
 #    })
 
 
-def player_results(request, player_id, match_number):
+def player_results(request, name, match_number):
     return render_to_response(
 
     'results.html',
     {
         'titlehead': "Results",
         'pagetitle': "Players results",
-        'matches':Match.objects.filter(player=player_id,match_number= match_number).all(),
+        'player_name': Player.objects.get(name=name),
+        'matches':Match.objects.filter(player=Player.objects.get(name=name),match_number= match_number).all(),
 
     })
 
