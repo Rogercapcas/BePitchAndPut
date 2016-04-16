1a43e4df71eb62ffcff1d0afbcbe311f750073f3
Rogercapcas <rogercapcas@gmail.com>
2016-04-16 19:21:04 +0200

    afegida funció results al views.py i fet l'html del results

--- BePitchAndPutt/iBePitchAndPutt/models.py
+++ BePitchAndPutt/iBePitchAndPutt/models.py
@@ -1,13 +1,12 @@
 from	django.db	import	models
 from	datetime	import	date
-from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 # Create your models here.
 
 class Player (models.Model):
-    name = models.ForeignKey('auth.User')
+    name = models.CharField(max_length=100)
     number_of_player = models.IntegerField()
     birthdate = models.DateField()
     stateOrProvince = models.TextField(blank=True, null=True)
@@ -18,7 +17,7 @@ class Player (models.Model):
     handicap = models.FloatField()
 
 class Field(models.Model):
-    fieldCode = models.IntegerField()
+    FieldCode = models.IntegerField()
     stateOrProvince = models.TextField(blank=True, null=True)
     city = models.TextField(max_length=100)
     street = models.TextField(blank=True, null=True)
@@ -28,12 +27,6 @@ class Field(models.Model):
     number_of_holes = models.IntegerField()
     par = models.IntegerField()
 
-<<<<<<< HEAD
-=======
-    def __unicode__(self):
-        return u"%s%" % Field.FieldCode
-
->>>>>>> 844833facfeb38c9b7ac79b76eaca1c5a4480a3c
 class Hole (models.Model):
     field = models.ForeignKey(Field)
     hole_number = models.IntegerField()
@@ -43,7 +36,7 @@ class Hole (models.Model):
 
 class Match (models.Model):
     match_number = models.IntegerField()
-    birthdate = models.DateField()
+    date = models.DateField()
     match_h = models.TextField()
     hole = models.ForeignKey(Hole)
     hole_result = models.IntegerField()
@@ -53,8 +46,6 @@ class Result (models.Model):
     match = models.ForeignKey(Match)
     match_result = models.IntegerField()
     handicap_variation = models.FloatField()
-    date = models.DateTimeField(default=timezone.now)
-
 
 class Rule (models.Model):
     ruls = models.TextField()
